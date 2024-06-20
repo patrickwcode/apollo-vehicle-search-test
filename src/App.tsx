af542beaa652903772137a286534a1713c7e10c7
Patrick Wauschek <patrick@patrickwcode.com>
2024-06-20 18:39:04 -0500

    Creates GetMakes query as component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,5 @@
 import "./App.css";
-import { gql, useQuery } from "@apollo/client";
-
-const query = gql`
-  query Makes {
-    makes @rest(type: "Make", path: "/api/makes?direction=asc&sort=id") {
-      data {
-        id
-        name
-      }
-    }
-  }
-`;
-
-function GetMakes() {
-  const { loading, error, data } = useQuery(query);
-  if (loading) return <option>Loading...</option>;
-  if (error) return <option>Error : {error.message}</option>;
-
-  return data.makes.data.map((make: any) => {
-    return (
-      <option id={make.id} key={make.id}>
-        {make.name}
-      </option>
-    );
-  });
-}
+import GetMakes from "./GetMakes";
 
 function App() {
   return (
---
+++ src/GetMakes.tsx
@@ -0,0 +1,30 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MAKES = gql`
+  query Makes {
+    makes @rest(type: "Make", path: "/api/makes?direction=asc&sort=id") {
+      data {
+        id
+        name
+      }
+    }
+  }
+`;
+
+function GetMakesQuery() {
+  const { loading, error, data } = useQuery(GET_MAKES);
+  if (loading) return <option>Loading...</option>;
+  if (error) return <option>Error : {error.message}</option>;
+
+  return data.makes.data.map((make: any) => {
+    return (
+      <option id={make.id} key={make.id}>
+        {make.name}
+      </option>
+    );
+  });
+}
+
+export default function GetMakes() {
+  return <GetMakesQuery />;
+}
