a491ca4027c83124a7225db7707e0397ca24f1b4
Patrick Wauschek <patrick@patrickwcode.com>
2024-06-22 12:03:14 -0500

    UseEffect for fetching years from Car API.

--- src/GetYears.tsx
+++ src/GetYears.tsx
@@ -1,26 +1,34 @@
+import { useEffect, useState } from "react";
 const CAR_API_KEY: string = import.meta.env.VITE_CAR_API_KEY;
 
 // Car API endpoint for Years does not have keys.  Only an array of years.
-function GetYearsFetch() {
-  try {
-    fetch("https://car-api2.p.rapidapi.com/api/years", {
-      method: "GET",
-      headers: {
-        "x-rapidapi-key": CAR_API_KEY,
-        "x-rapidapi-host": "car-api2.p.rapidapi.com",
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        return json.map((year: string) => {
-          return <option>{year}</option>;
-        });
-      });
-  } catch (err) {
-    console.error(err);
-  }
-}
 
 export default function GetYears() {
-  return <GetYearsFetch />;
+  let [years, setYears] = useState<object>({});
+
+  useEffect(() => {
+    try {
+      fetch("https://car-api2.p.rapidapi.com/api/years", {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key": CAR_API_KEY,
+          "x-rapidapi-host": "car-api2.p.rapidapi.com",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          setYears(json);
+        });
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  if (years !== null) {
+    return Object.keys(years).map((year: string) => {
+      return <option>{year}</option>;
+    });
+  } else {
+    return <option>Error: No years found.</option>
+  }
 }
